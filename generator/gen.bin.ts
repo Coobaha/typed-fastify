692e13e2b57ef98db6bfe51e0967ad682b24362a
Alexander Ryzhikov <coobaha@gmail.com>
2021-02-17 17:29:25 +0200

    feat: add schema generator

---
+++ generator/gen.bin.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env ts-node
+import yargs from 'yargs';
+
+// @ts-ignore
+import { hideBin } from 'yargs/helpers';
+import gen from './gen';
+import glob from 'glob';
+import { promisify } from 'util';
+const resolveGlobFiles = promisify(glob);
+
+yargs(hideBin(process.argv))
+  .command<{ files: string }>(
+    'gen [files]',
+    'Generates json schemas next to corresponding ts files',
+    (yargs) => {
+      yargs
+        .positional('files', {
+          describe: 'glob pattern of files',
+          type: 'string',
+          demandOption: true,
+        })
+        .demandOption(['files']);
+    },
+    async (argv) => {
+      const files = await resolveGlobFiles(argv.files);
+
+      await gen({ files });
+    },
+  )
+  .demandCommand(1, 'You need at least one command before moving on')
+  .strict().argv;
